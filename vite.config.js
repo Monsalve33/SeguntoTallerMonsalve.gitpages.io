85a9d5a75e46f41c83b2675ac09d1c39d7b31b39
MonsalveeeGod <edwardarrietamonsa@gmail.com>
2026-04-13 20:10:23 -0500

    change git

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SeguntoTallerMonsalve.gitpages.io/',
+  base: '/Segundo-Taller-React/',
   plugins: [
     react({
       babel: {
